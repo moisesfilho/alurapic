1df489bde6ac714e7777f81e1d932718be9ef495
moisesfilho <moises.son@gmail.com>
2020-12-12 22:23:18 -0300

    Organizando os módulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Photo } from './photos/photo/photo';
 import { PhotoService } from './photos/photo/photo.service';
 
@@ -7,10 +7,12 @@ import { PhotoService } from './photos/photo/photo.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AluraPic';
   photos: Photo[] = [];
-  constructor(photoService: PhotoService) {
-    photoService.listFromUser('flavio').subscribe((photos) => (this.photos = photos));
+  constructor(private photoService: PhotoService) {}
+
+  ngOnInit(): void {
+    this.photoService.listFromUser('flavio').subscribe((photos) => (this.photos = photos));
   }
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [ PhotoComponent ],
-  imports: [ CommonModule ],
-  exports: [ PhotoComponent ],
+  declarations: [PhotoComponent],
+  imports: [CommonModule, HttpClientModule],
+  exports: [PhotoComponent],
   providers: [],
 })
 export class PhotosModule {}

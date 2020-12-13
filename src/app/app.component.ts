04223d1e1bfbe57ab2c2d609b32ebdd8fc15de5f
moisesfilho <moises.son@gmail.com>
2020-12-12 21:46:49 -0300

    Consumindo api de forma simples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AluraPic';
-  photos = [];
+  photos: Object[] = [];
   constructor(http: HttpClient) {
-
+    http.get<Object[]>('http://localhost:3000/flavio/photos').subscribe((photos) => (this.photos = photos));
   }
 }

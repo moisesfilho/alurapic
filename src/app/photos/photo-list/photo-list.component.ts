4847481b5c17546e07bc8fde5ace7442bbc760d3
moisesfilho <moises.son@gmail.com>
2021-01-02 17:38:19 -0300

    Atualizando atributo do componente com evento view

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -9,8 +9,8 @@ import { PhotoService } from '../photo/photo.service';
   styleUrls: ['./photo-list.component.css'],
 })
 export class PhotoListComponent implements OnInit {
-  title = 'AluraPic';
   photos: Photo[] = [];
+  filter: string = '';
   constructor(private photoService: PhotoService, private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {

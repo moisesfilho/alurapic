603b484b1ab11727c6feb8aa48fbef4cd3b99d0d
moisesfilho <moises.son@gmail.com>
2021-01-02 16:25:29 -0300

    Criando novos componentes e utilizando roteamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Photo } from './photos/photo/photo';
-import { PhotoService } from './photos/photo/photo.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'AluraPic';
-  photos: Photo[] = [];
-  constructor(private photoService: PhotoService) {}
-
-  ngOnInit(): void {
-    this.photoService.listFromUser('flavio').subscribe((photos) => (this.photos = photos));
-  }
+export class AppComponent {
+  
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
 
 @NgModule({
-  declarations: [PhotoComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
   imports: [CommonModule, HttpClientModule],
-  exports: [PhotoComponent],
   providers: [],
 })
 export class PhotosModule {}

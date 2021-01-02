86658477f3643d8a20b03f2171ad349edf4e16ef
moisesfilho <moises.son@gmail.com>
2021-01-02 17:17:22 -0300

    Componente baseado em mudanças nas propriedades

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,9 +4,10 @@ import { PhotoComponent } from './photo/photo.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotosComponent } from './photo-list/photos/photos.component';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
+  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
   imports: [CommonModule, HttpClientModule],
   providers: [],
 })

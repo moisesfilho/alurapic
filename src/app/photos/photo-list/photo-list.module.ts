b8bf722aefb31f3da6b2810ce7b666f8521c7bb7
moisesfilho <moises.son@gmail.com>
2021-01-03 16:14:50 -0300

    Criando diretiva para aplicar estilo

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CardModule } from 'src/app/shared/components/card/card.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
 import { PhotoModule } from '../photo/photo.module';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { LoadButtonComponent } from './load-button/load-button.component';
@@ -10,6 +11,6 @@ import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [PhotoListComponent, PhotosComponent, LoadButtonComponent, FilterByDescription, SearchComponent],
-  imports: [CommonModule, PhotoModule, CardModule],
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 })
 export class PhotoListModule {}

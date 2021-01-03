e6cf6122e716765dcd358ebc47cf510db6c465ec
moisesfilho <moises.son@gmail.com>
2021-01-03 16:29:08 -0300

    Criando componente de login e home

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SingInComponent } from './singin/singin.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SingInComponent]
+})
+export class HomeModule { }
---
+++ src/app/home/singin/singin.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './singin.component.html',
+  styleUrls: ['./singin.component.css']
+})
+export class SingInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

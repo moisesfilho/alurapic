8326486be6f05b26ac25a19f967955eda8942ec1
moisesfilho <moises.son@gmail.com>
2021-01-16 18:18:31 -0300

    Criando formulário de cadastro

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { SingInComponent } from './singin/singin.component';
+import { SingUpComponent } from './singup/singup.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule],
-  declarations: [SingInComponent],
+  imports: [CommonModule, ReactiveFormsModule, VMessageModule, RouterModule, FormsModule],
+  declarations: [SingInComponent, SingUpComponent],
 })
 export class HomeModule {}
---
+++ src/app/home/singup/singup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './singup.component.html',
+  styleUrls: ['./singup.component.css']
+})
+export class SingUpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

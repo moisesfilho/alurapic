d66157c59e6985d2849f68e4000bfa0e54cea109
moisesfilho <moises.son@gmail.com>
2021-01-16 17:34:43 -0300

    BehaviorSubject para guardar valor até o consumo

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { TokenService } from '../token/token.service';
 import { User } from './user';
 import * as jwt_decode from 'jwt-decode';
@@ -8,7 +8,7 @@ import * as jwt_decode from 'jwt-decode';
   providedIn: 'root',
 })
 export class UserService {
-  private userSubject = new Subject<User>();
+  private userSubject = new BehaviorSubject<User>(null);
 
   constructor(private tokenService: TokenService) {
     this.tokenService.hasToken() && this.decodeAndNotify();

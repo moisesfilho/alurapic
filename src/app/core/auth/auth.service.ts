3c28f4a60ae3be5e6b20e47571305de0de6ea8a7
moisesfilho <moises.son@gmail.com>
2021-01-16 16:40:25 -0300

    Capturando header de autenticação com pipe

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 const API_URL = 'http://localhost:3000/';
 
@@ -9,6 +10,13 @@ const API_URL = 'http://localhost:3000/';
 export class AuthService {
   constructor(private http: HttpClient) {}
   authenticate(userName: string, password: string) {
-    return this.http.post(API_URL + 'user/login', { userName, password });
+    return this.http.post(API_URL + 'user/login', { userName, password }, { observe: 'response' })
+                    .pipe(
+                      tap((res) => {
+                        const authToken = res.headers.get('x-access-token');
+                        window.localStorage.setItem('authToken', authToken);
+                        console.log(`User ${userName} authenticated with token ${authToken}`);
+                      })
+                    );
   }
 }

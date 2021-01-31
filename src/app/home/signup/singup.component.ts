37a0533aac222a67af07aa24f9ff999ee9623c10
moisesfilho <moises.son@gmail.com>
2021-01-31 17:01:46 -0300

    Validação integrada com serviço

--- src/app/home/signup/singup.component.ts
+++ src/app/home/signup/singup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 
 @Component({
   templateUrl: './singup.component.html',
@@ -9,13 +10,17 @@ import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validat
 export class SignUpComponent implements OnInit {
   signupForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private userNotTakenValidatorService: UserNotTakenValidatorService) {}
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-      userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)]],
+      userName: [
+        '',
+        [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)],
+        this.userNotTakenValidatorService.checkUserNameTaken(),
+      ],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
     });
   }

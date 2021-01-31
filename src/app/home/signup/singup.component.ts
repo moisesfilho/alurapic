7b5e5f82bc2ba5fbf20b13d42ed9de84a5ba8e1f
moisesfilho <moises.son@gmail.com>
2021-01-31 16:18:41 -0300

    Criando validador customizado

--- src/app/home/signup/singup.component.ts
+++ src/app/home/signup/singup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
   templateUrl: './singup.component.html',
@@ -14,7 +15,7 @@ export class SignUpComponent implements OnInit {
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(40)]],
-      userName: ['', [Validators.required, Validators.pattern(/^[a-z0-9_\-]+$/), Validators.minLength(2), Validators.maxLength(30)]],
+      userName: ['', [Validators.required, lowerCaseValidator, Validators.minLength(2), Validators.maxLength(30)]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
     });
   }
